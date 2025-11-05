daf6213b3fde8a3232441a33dcaf50f7264636d1
Tarik Gadoumi <tarik_gadoumi@hotmail.fr>
2025-11-05 07:08:51 +0100

    feat(api): expose CRUD Product via ModelViewSet (tri configurable)

--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, filters
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by("-created_at")
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.AllowAny]  # à durcir plus tard
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ["created_at", "price", "name"]
+    ordering = ["-created_at"]
