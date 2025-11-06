64aa1c27697be3a6e1dc0026bc0ccf24ea4ab6e3
Tarik Gadoumi <tarik_gadoumi@hotmail.fr>
2025-11-06 08:21:21 +0100

    feat(products): support XML & JSON via parser_classes/renderer_classes on ProductViewSet

--- products/views.py
+++ products/views.py
@@ -3,7 +3,10 @@ from .models import Product
 from .serializers import ProductSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import extend_schema, OpenApiExample, OpenApiParameter
-
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+from rest_framework_xml.parsers import XMLParser
+from rest_framework_xml.renderers import XMLRenderer
 
 @extend_schema(
     tags=["Products"],
@@ -39,8 +42,9 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all().order_by("-created_at")
     serializer_class = ProductSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    filter_backends = [filters.OrderingFilter]
+    parser_classes = [JSONParser, XMLParser]
+    renderer_classes = [JSONRenderer, XMLRenderer]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     ordering_fields = ["created_at", "price", "name"]
     ordering = ["-created_at"]
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     filterset_fields = ["name", "price"]
